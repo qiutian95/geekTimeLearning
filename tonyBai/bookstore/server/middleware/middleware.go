14facb5020233144bedcf507d6dcb081a0896338
wangjian <2861141635@qq.com>
2023-10-24 10:42:34 +0800

    添加些注释

--- tonyBai/bookstore/server/middleware/middleware.go
+++ tonyBai/bookstore/server/middleware/middleware.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// 中间件使用方式的一种，传入一个参数，返回一个参数
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("rece a %s request from %s", r.Method, r.RemoteAddr)
