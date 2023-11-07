ea4fdd6583c042f866ea8de941d7f4dcac33fe72
wangjian <2861141635@qq.com>
2023-11-07 10:32:44 +0800

    channel 测试缓冲和无缓冲的收发性能

---
+++ tonyBai/concurrency/channel/cache/messagequeue/buffered/one_to_one_100_test.go
@@ -0,0 +1,43 @@
+package buffered
+
+import "testing"
+
+var c1 chan string
+var c2 chan string
+
+func init() {
+
+	c1 = make(chan string, 100)
+	go func() {
+		for {
+			<-c1
+		}
+	}()
+
+	c2 = make(chan string, 100)
+	go func() {
+		for {
+			c2 <- "hello"
+		}
+	}()
+}
+
+func send(s string) {
+	c1 <- s
+}
+
+func receive() {
+	<-c2
+}
+
+func BenchmarkUnbuffered1to1Send(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		send("aaa")
+	}
+}
+
+func BenchmarkUnbuffered1toReceive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		receive()
+	}
+}
